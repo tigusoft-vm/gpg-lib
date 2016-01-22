93938f4d9a2dcdb63280f1a1773f16aaedcd5fad
rob <rob@mail.l>
2016-01-22 12:25:59 +0000

    verify file (seg fault)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+#include <cstdio>
 #include <iostream>
 #include <gpgme.h>
 
 int main(int argc, char **argv) {
-	gpgme_signature_t sig;
+	gpgme_ctx_t ctx;
+	gpgme_new(&ctx);
+
+	gpgme_data_t data_file;
+	FILE *dataFileDescriptor;
+	fopen("test.txt", "r");
+	gpgme_data_new_from_fd(&data_file, fileno(dataFileDescriptor));
+
+	gpgme_data_t sig_file;
+	FILE *sigFileDescriptor;
+	fopen("test.txt.sig", "r");
+	gpgme_data_new_from_fd(&sig_file, fileno(sigFileDescriptor));
+
+	gpgme_op_verify(ctx, sig_file, data_file, nullptr);
+	gpgme_release(ctx);
     return 0;
 }
