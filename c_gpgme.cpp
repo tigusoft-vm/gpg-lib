0167e299aebaf9b60f1fc45e80e4a60fa041977e
rob <rob@mail.l>
2016-02-04 16:40:30 +0000

    [wip] load file

--- c_gpgme.cpp
+++ c_gpgme.cpp
@@ -1,6 +1,8 @@
 #include "c_gpgme.hpp"
 
 #include <cassert>
+#include <fcntl.h>
+#include <stdexcept>
 
 c_gpgme::c_gpgme() {
 	setlocale (LC_ALL, "");
@@ -12,3 +14,20 @@ c_gpgme::c_gpgme() {
 	assert(m_ctx != nullptr);
 }
 
+gpgme_data_t c_gpgme::load_file ( const std::string &filename ) {
+	gpgme_data_t data_file = nullptr;
+#ifdef __linux__
+	int file_descriptor = open(filename.c_str(), O_RDONLY); // TODO
+	if (file_descriptor == -1) {
+		throw std::runtime_error(std::string("cannot open file ") + filename);
+	}
+#endif
+	m_error_code = gpgme_data_new_from_fd(&data_file, file_descriptor);
+	
+	return data_file;
+}
+
+
+c_gpgme::~c_gpgme() {
+	gpgme_release(m_ctx);
+}
--- c_gpgme.hpp
+++ c_gpgme.hpp
@@ -13,11 +13,23 @@ class c_gpgme
 		c_gpgme & operator=(const c_gpgme &) = delete;
 		c_gpgme & operator=(c_gpgme &&) = delete;
 
+		/**
+		 * Verify signed message. @param sig_file is detached signature file (gpg --detach-sign)
+		 * @param clear_data_file is signed clear data filename.
+		 */
+		bool verify_detached_signature(const std::string &sig_file, const std::string &clear_data_file);
+
+		/**
+		 * Verify signed message from @param sig_file and save clear data to @param output_data_file
+		 */
+		bool verify_clearsign_file(const std::string &sig_file, const std::string &output_data_file);
+
 		~c_gpgme();
 	private:
 		gpgme_ctx_t m_ctx = nullptr;
 		gpgme_error_t m_error_code;
 
+		gpgme_data_t load_file(const std::string &filename);
 };
 
 #endif // C_GPGME_H
