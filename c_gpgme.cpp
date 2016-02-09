45c31ee48c30f9931937f602e256cb9b534481b1
win <win@example.com>
2016-02-09 13:42:00 +0100

    remove debug

--- c_gpgme.cpp
+++ c_gpgme.cpp
@@ -2,11 +2,8 @@
 
 #include <cassert>
 #include <fcntl.h>
-#include <iostream>
 #include <stdexcept>
 
-// TODO delete me
-#define dbg(X) std::cout << __LINE__ << ": " << X << std::endl
 
 #ifdef __CYGWIN__
 #include <sstream>
@@ -29,7 +26,6 @@ c_gpgme::c_gpgme() {
 	assert(m_ctx != nullptr);
 	m_error_code = gpgme_ctx_set_engine_info (m_ctx, GPGME_PROTOCOL_OpenPGP,
                "./gpg", nullptr);
-	dbg("error " << m_error_code);
 }
 
 
@@ -50,7 +46,7 @@ bool c_gpgme::verify_detached_signature ( const std::string &sig_file, const std
 			}
 			else if (sig->summary == 0 && sig->status == GPG_ERR_NO_ERROR) { // Valid but key is not certified with a trusted signature
 				return true;
-			} 
+			}
 		}
 		return false;
 	}
@@ -66,12 +62,10 @@ std::unique_ptr<gpgme_data_t, std::function<void(gpgme_data_t *)>> c_gpgme::load
 		delete ptr;
 	};
 	std::unique_ptr<gpgme_data_t, std::function<void(gpgme_data_t *)>> data_file_ptr(new gpgme_data_t, deleter);
-//#ifdef __linux__ // TODO windows
 	int file_descriptor = open(filename.c_str(), O_RDONLY);
 	if (file_descriptor == -1) {
 		throw std::runtime_error(std::string("cannot open file ") + filename);
 	}
-//#endif
 	m_error_code = gpgme_data_new_from_fd(data_file_ptr.get(), file_descriptor);
 	if (m_error_code) {
 		throw std::runtime_error(std::string("load file error, error code ") + std::to_string(m_error_code));
@@ -82,28 +76,7 @@ std::unique_ptr<gpgme_data_t, std::function<void(gpgme_data_t *)>> c_gpgme::load
 
 void c_gpgme::load_public_key ( const std::string &filename ) {
 	auto key_file_ptr = load_file(filename);
-	//gpgme_data_t data_file = nullptr;
-	//int dataFileDescriptor = open(filename.c_str(), O_RDONLY);
-	//m_error_code = gpgme_data_new_from_fd(&data_file, dataFileDescriptor);
 	m_error_code = gpgme_op_import(m_ctx, *key_file_ptr);
-	//gpgme_data_release(data_file);
-
-	dbg("**************************");
-	gpgme_import_result_t key_import_result = gpgme_op_import_result(m_ctx);
-	dbg("considered " << key_import_result->considered);
-	dbg("no_user_id " << key_import_result->no_user_id);
-	dbg("imported_rsa " << key_import_result->imported_rsa);
-	dbg("unchanged " << key_import_result->unchanged);
-	dbg("new_user_ids " << key_import_result->new_user_ids);
-	dbg("new_sub_keys " << key_import_result->new_sub_keys);
-	dbg("new_signatures " << key_import_result->new_signatures);
-	dbg("new_revocations " << key_import_result->new_revocations);
-	dbg("secret_read " << key_import_result->secret_read);
-	dbg("secret_imported " << key_import_result->secret_imported);
-	dbg("secret_unchanged " << key_import_result->secret_unchanged);
-	dbg("not_imported " << key_import_result->not_imported);
-
-	dbg("**************************");
 }
 
 
