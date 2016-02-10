a8a34c7b5d1cc27a40baaa7420235203a1e5d9a0
rob <rob@mail.l>
2016-02-10 12:43:15 +0000

    exceptions

--- c_gpgme.cpp
+++ c_gpgme.cpp
@@ -1,5 +1,6 @@
 #include "c_gpgme.hpp"
 
+#include <boost/filesystem.hpp>
 #include <cassert>
 #include <fcntl.h>
 #include <stdexcept>
@@ -32,35 +33,33 @@ c_gpgme::c_gpgme() {
 
 
 bool c_gpgme::verify_detached_signature ( const std::string &sig_file, const std::string &clear_data_file, const std::string &expected_fpr ) {
-	try {
-		auto sig_file_ptr = load_file(sig_file);
-		auto clear_data_file_ptr = load_file(clear_data_file);
-		m_error_code = gpgme_op_verify(m_ctx, *sig_file_ptr, *clear_data_file_ptr, nullptr);
-		if (m_error_code) return false;
-		gpgme_verify_result_t result = gpgme_op_verify_result(m_ctx);
-		if (result == nullptr) return false;
-		gpgme_signature_t sig = result->signatures;
-		if (!sig) return false;
+	auto sig_file_ptr = load_file(sig_file);
+	auto clear_data_file_ptr = load_file(clear_data_file);
+	m_error_code = gpgme_op_verify(m_ctx, *sig_file_ptr, *clear_data_file_ptr, nullptr);
+	if (m_error_code) return false;
+	gpgme_verify_result_t result = gpgme_op_verify_result(m_ctx);
+	if (result == nullptr) return false;
+	gpgme_signature_t sig = result->signatures;
+	if (!sig) return false;
 
-		for (; sig; sig = sig->next) {
-			if ((sig->summary & GPGME_SIGSUM_VALID) || (sig->summary & GPGME_SIGSUM_GREEN)) {  // Valid
-				if (expected_fpr == sig->fpr) return true;
-				else return false;
-			}
-			else if (sig->summary == 0 && sig->status == GPG_ERR_NO_ERROR) { // Valid but key is not certified with a trusted signature
-				if (expected_fpr == sig->fpr) return true;
-				else return false;
-			}
+	for (; sig; sig = sig->next) {
+		if ((sig->summary & GPGME_SIGSUM_VALID) || (sig->summary & GPGME_SIGSUM_GREEN)) {  // Valid
+			if (expected_fpr == sig->fpr) return true;
+			else return false;
 		}
-		return false;
+		else if (sig->summary == 0 && sig->status == GPG_ERR_NO_ERROR) { // Valid but key is not certified with a trusted signature
+			if (expected_fpr == sig->fpr) return true;
+			else return false;
+		}
 	}
-	catch(std::exception e) {
-		return false;
-	}
+	return false;
 }
 
 
 std::unique_ptr<gpgme_data_t, std::function<void(gpgme_data_t *)>> c_gpgme::load_file ( const std::string &filename ) {
+	if (!boost::filesystem::exists(filename)) {
+		throw std::runtime_error(std::string("file not exists"));
+	}
 	auto deleter = [](gpgme_data_t *ptr) {
 		gpgme_data_release(*ptr);
 		delete ptr;
@@ -81,36 +80,16 @@ std::unique_ptr<gpgme_data_t, std::function<void(gpgme_data_t *)>> c_gpgme::load
 void c_gpgme::remove_key_from_keyring ( const std::string &fingerprint ) {
 	gpgme_key_t key;
 	m_error_code = gpgme_get_key(m_ctx, fingerprint.c_str(), &key, 0);
-	if (m_error_code == GPG_ERR_EOF) {
-		throw std::runtime_error("Key is not found in the keyring");
-	}
-	else if (m_error_code == GPG_ERR_INV_VALUE) {
-		throw std::runtime_error("Ctx or r_key is not a valid pointer or fpr is not a fingerprint or key ID");
-	}
-	else if (m_error_code == GPG_ERR_AMBIGUOUS_NAME) {
-		throw std::runtime_error("key ID was not a unique specifier for a key");
-	}
-	else if (m_error_code == GPG_ERR_ENOMEM) {
-		throw std::runtime_error("not enough memory available");
-	}
 
-	assert(m_error_code == GPG_ERR_NO_ERROR);
+	if (m_error_code != GPG_ERR_NO_ERROR) {
+		throw std::runtime_error(gpgme_strsource(m_error_code));
+	}
 
 	m_error_code = gpgme_op_delete(m_ctx, key, 0);
-	if (m_error_code == GPG_ERR_INV_VALUE) {
-		throw std::runtime_error("ctx or key is not a valid pointer");
-	}
-	else if (m_error_code == GPG_ERR_NO_PUBKEY) {
-		throw std::runtime_error("key could not be found in the keyring");
-	}
-	else if (m_error_code == GPG_ERR_AMBIGUOUS_NAME) {
-		throw std::runtime_error("key was not specified unambiguously");
-	}
-	else if (m_error_code == GPG_ERR_CONFLICT) {
-		throw std::runtime_error("secret key for key is available, but allow_secret is zero");
-	}
 
-	assert(m_error_code == GPG_ERR_NO_ERROR);
+	if (m_error_code != GPG_ERR_NO_ERROR) {
+		throw std::runtime_error(gpgme_strsource(m_error_code));
+	}
 }
 
 
